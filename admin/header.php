3ffcb849fe6686006691fafb2453fb4b8e7d4565
Carlos Vargas <info-analista7@iancarina.com.ve>
2017-01-20 09:15:51 -0400

    upload the other files form views

---
+++ admin/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>..:: AutoCrud ::..</title>
+	<link rel="stylesheet" type="text/css" href="<?php print assets(); ?>plugins/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php print assets(); ?>plugins/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php print assets(); ?>plugins/DataTables-1.10.12/media/css/jquery.dataTables.min.css">
+</head>
+<body>
+
+<!--Javascript Files Zone-->
+<script type="text/javascript" src="<?php print assets(); ?>js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="<?php print assets(); ?>plugins/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php print assets(); ?>plugins/DataTables-1.10.12/media/js/jquery.dataTables.min.js"></script>
+<!--End Javascript Files Zone-->
+</body>
+</html>
