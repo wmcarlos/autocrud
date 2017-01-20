95df029225aedffeb489e2f3a1466509a9e4543d
Carlos Vargas <info-analista7@iancarina.com.ve>
2017-01-20 15:31:21 -0400

    load the first data from list form

--- admin/db/autocrud.sql
+++ admin/db/autocrud.sql
@@ -15,7 +15,7 @@ create table list(
 create table value(
 	value_id int auto_increment not null,
 	list_id int not null,
-	parent_value_id int not null,
+	parent_value_id int null,
 	created timestamp not null default current_timestamp,
 	updated timestamp null,
 	default_value text,
@@ -46,6 +46,7 @@ create table field(
 	field_id int auto_increment not null,
 	columname varchar(150) not null,
 	field_type_id int not null,
+	isstatic char(1) not null default 'Y',
 	static_list_id int null,
 	dinamic_query_id int null,
 	parent_field_id int null,
@@ -58,10 +59,7 @@ create table field(
 	conditions text,
 	isactive char(1) not null default 'Y',
 	constraint pk_field_id primary key (field_id),
-	constraint fk_field_type_id foreign key (field_type_id) references value (value_id) on update cascade on delete restrict,
-	constraint fk_static_list_id foreign key (static_list_id) references value (value_id) on update cascade on delete restrict,
-	constraint fk_dinamic_query_id foreign key (dinamic_query_id) references value (value_id) on update cascade on delete restrict,
-	constraint fk_parent_field_id foreign key (parent_field_id) references field (field_id) on update cascade on delete restrict
+	constraint fk_field_type_id foreign key (field_type_id) references value (value_id) on update cascade on delete restrict
 )engine = InnoDB;
 
 create table element(
@@ -81,8 +79,8 @@ create table element(
 create table item(
 	item_id int auto_increment not null,
 	item_type_id int not null,
-	window_id int not null,
-	parent_item_id int not null,
+	window_id int null,
+	parent_item_id int null,
 	menu_id int not null,
 	created timestamp not null default current_timestamp,
 	updated timestamp null,
@@ -95,8 +93,6 @@ create table item(
 	isactive char(1) not null default 'Y',
 	constraint pk_item_id primary key (item_id),
 	constraint fk_item_type_id foreign key (item_type_id) references value (value_id) on update cascade on delete restrict,
-	constraint fk_window_id001 foreign key (window_id) references window (window_id) on update cascade on delete restrict,
-	constraint fk_parent_item_id foreign key (parent_item_id) references item (item_id) on update cascade on delete restrict,
 	constraint fk_menu_id foreign key (menu_id) references value (value_id) on update cascade on delete restrict
 )engine = InnoDB;
 
@@ -167,24 +163,73 @@ create table post(
 	constraint fk_user_id002 foreign key (user_id) references user (user_id) on update cascade on delete restrict
 )engine=InnoDB;
 
-#list
+#LIST
 insert into list(list_id,name) values (1,"ROLES");
+insert into list(list_id,name) values (2,"WINDOWS TYPE");
+insert into list(list_id,name) values (3,"FIELDS TYPE");
+insert into list(list_id,name) values (4,"PRIVATE MENU");
+insert into list(list_id,name) values (5,"PUBLIC MENU");
+insert into list(list_id,name) values (6,"ITEM TYPE");
+insert into list(list_id,name) values (7,"ELEMENT TYPE");
 
-#value
+#VALUE
 insert into value (value_id,list_id,default_value) values (1,1,"SYSTEM");
 insert into value (value_id,list_id,default_value) values (2,1,"ADMIN");
+#WINDOW TYPE
+insert into value (value_id,list_id,default_value) values (3,2,"MASTER FORM");
+insert into value (value_id,list_id,default_value) values (4,2,"DETAIL FORM");
+#FIELD TYPE
+insert into value (value_id,list_id,default_value) values (5,3,"TEXT");
+insert into value (value_id,list_id,default_value) values (6,3,"SELECT");
+insert into value (value_id,list_id,default_value) values (7,3,"RADIO");
+insert into value (value_id,list_id,default_value) values (8,3,"CHECKBOX");
+insert into value (value_id,list_id,default_value) values (9,3,"TEXTAREA");
+insert into value (value_id,list_id,default_value) values (10,3,"DATE");
+insert into value (value_id,list_id,default_value) values (11,3,"AUTO-COMPLETE");
+insert into value (value_id,list_id,default_value) values (12,3,"DATE-TIME");
+#PRIVATE LIST
+insert into value (value_id,list_id,default_value) values (13,5,"GENERAL");
+#ITEM TYPE
+insert into value (value_id,list_id,default_value) values (14,6,"FOLDER");
+insert into value (value_id,list_id,default_value) values (15,6,"WINDOW");
+insert into value (value_id,list_id,default_value) values (16,6,"REPORT");
+insert into value (value_id,list_id,default_value) values (17,6,"CHART");
+#ELEMENT TYPE
+insert into value (value_id,list_id,default_value) values (18,7,"FIELD");
+insert into value (value_id,list_id,default_value) values (19,7,"DETAIL FORM");
 
-#user
+#USER
 insert into user (user_id, role_id, first_name,last_name,sex,birthday,email,phone,username)
 	values (1,1,"SYSTEM NAME","SYSTEM LAST NAME",'M',"1991-01-09","systememail@domain.com","+580000000000","SYSTEM");
 
-#password
-insert into password (password_id,user_id,strpassword)
-	values (1,1,MD5("4ut0crud"));
+#PASSWORD
+insert into password (password_id,user_id,strpassword) values (1,1,MD5("4ut0crud"));
 
-#question
-insert into question(question_id,user_id,strquestion,stranswer)
-	values (1,1,"auto","crud");
+#QUESTION
+insert into question(question_id,user_id,strquestion,stranswer) values (1,1,"auto","crud");
+insert into question(question_id,user_id,strquestion,stranswer)values (2,1,"crud","auto");
 
-insert into question(question_id,user_id,strquestion,stranswer)
-	values (2,1,"crud","auto");
+#WINDOWS
+insert into window (window_id,window_type_id,tablename,name) values (1,3,"list","LIST");
+
+ #FIELD
+insert into field (field_id,columname,field_type_id,isstatic,name) values (1,"list_id",6,"Y","LIST ID");
+insert into field (field_id,columname,field_type_id,isstatic,name) values (2,"created",12,"Y","CREATED");
+insert into field (field_id,columname,field_type_id,isstatic,name) values (3,"updated",12,"Y","UPDATED");
+insert into field (field_id,columname,field_type_id,isstatic,name) values (4,"name",5,"Y","NAME");
+insert into field (field_id,columname,field_type_id,isstatic,name) values (5,"description",9,"Y","DESCRIPTION");
+insert into field (field_id,columname,field_type_id,isstatic,name) values (6,"isactive",9,"Y","ISACTIVE");
+
+#ELEMENT
+insert into element (element_id,element_type_id,window_id,identified,position) values (1,18,1,1,1);
+insert into element (element_id,element_type_id,window_id,identified,position) values (2,18,1,2,2);
+insert into element (element_id,element_type_id,window_id,identified,position) values (3,18,1,3,3);
+insert into element (element_id,element_type_id,window_id,identified,position) values (4,18,1,4,4);
+insert into element (element_id,element_type_id,window_id,identified,position) values (5,18,1,5,5);
+insert into element (element_id,element_type_id,window_id,identified,position) values (6,18,1,6,6);
+
+#ITEM
+insert into item (item_id,item_type_id,window_id,menu_id,name,position) values (1,15,1,13,"LIST",1);
+
+#ACCESS
+insert into access (access_id,item_id,role_id) values (1,1,1);
